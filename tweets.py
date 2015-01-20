88150150635800dee111b81f35496b617e5ab6d1
Raphaël Bournhonesque <raphael.bournhonesque@ens-lyon.fr>
2015-01-20 22:33:56 +0100

    Version qui marche

--- tweets.py
+++ tweets.py
@@ -41,6 +41,7 @@ class Tweet:
         if not self.authenticated:
             self.api = TwitterStream(auth=OAuth(**self.credentials))
             self.authenticated = True
+            logging.info("Authenticated.")
 
     def sample(self):
         """Return the tweets sampled by the Twitter sample API.
@@ -82,9 +83,10 @@ class Tweet:
                 return False
 
             if not isinstance(tweet[field], str) or tweet[field] == "":
-                logger.debug("The tweet failed the test because the {} field"
-                             "was incorrect: {}".format(field, tweet[field]))
-                return False
+                if field != "place":
+                    logger.debug("The tweet failed the test because the {} field"
+                                 "was incorrect: {}".format(field, tweet[field]))
+                    return False
 
         if not hasattr(tweet["place"], "__getitem__"):
             logger.debug("The tweet failed the test because the 'place' field is not iterable.")
@@ -115,23 +117,28 @@ class Tweet:
         dbname: str
                A string corresponding to the Sqlite file database where the data must be stored.
         """
+        logging.info("Creating database {}.".format(dbname))
+        self.database = dbname
         conn = sqlite3.connect(dbname)
         c = conn.cursor()
 
         c.execute("""CREATE TABLE PLACE
                      (PLACE_ID VARCHAR(50) NOT NULL PRIMARY KEY,
                       COUNTRY_CODE VARCHAR(5) NOT NULL,
-                      NAME VARCHAR(50) NOT NULL)
+                      NAME VARCHAR(50) NOT NULL);
                   """)
 
         c.execute("""CREATE TABLE TWEET
                      (TWEET_ID VARCHAR(50) NOT NULL PRIMARY KEY,
                       CREATED_AT VARCHAR(50) NOT NULL,
                       LANG VARCHAR (5) NOT NULL,
-                      PLACE_ID VARCHAR(50) NOT NULL FOREIGN KEY REFERENCES PLACE (PLACE_ID))
+                      PLACE_ID VARCHAR(50) NOT NULL,
+                      FOREIGN KEY(PLACE_ID) REFERENCES PLACE(PLACE_ID));
                  """)
+        conn.commit()
+        conn.close()
 
-    def record(self, tweet):
+    def record_tweet(self, tweet):
         """Record the input tweet in the given database.
 
         Parameters:
@@ -139,17 +146,43 @@ class Tweet:
         tweet: dictionary
               A dictionary corresponding to the tweet in the JSON format.
         """
+        logger.info("Recording tweet {}.".format(tweet["id_str"]))
         if self.database is None:
             raise Exception("No database was created, please create a database before starting to record tweets.")
 
         conn = sqlite3.connect(self.database)
         c = conn.cursor()
+        place_exists = c.execute("""SELECT * FROM PLACE WHERE PLACE_ID = ?""", (tweet["place"]["id"], )).fetchone()
+
+        if place_exists is None:
+            c.execute("""INSERT INTO PLACE VALUES(?, ?, ?)""", (tweet["place"]["id"],
+                                                                tweet["place"]["country_code"],
+                                                                tweet["place"]["name"]
+                                                                ))
+
+        c.execute("""INSERT INTO TWEET VALUES(?, ?, ?, ?)""", (tweet["id_str"],
+                                                               tweet["created_at"],
+                                                               tweet["lang"],
+                                                               tweet["place"]["id"]
+                                                               ))
+
+        conn.commit()
+        conn.close()
+
+    def record(self):
+        """Record the tweets from the sample Twitter API in the database."""
+        logging.info("Starting recording tweets...")
+        for tweet in self.sample():
+            if self.check_tweet(tweet):
+                self.record_tweet(tweet)
 
 
 if __name__ == "__main__":
     tweets_grabber = Tweet(credentials)
     tweets_grabber.authenticate()
+    tweets_grabber.create_database("test.db")
+    tweets_grabber.record()
 
-    for tweet in tweets_grabber.sample():
-        if tweets_grabber.check_tweet(tweet):
-            print(tweet["place"]["url"])
+    # for tweet in tweets_grabber.sample():
+    #     if tweets_grabber.check_tweet(tweet):
+    #         print(tweet["place"]["url"])
