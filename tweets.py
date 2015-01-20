6fe77b23b0cfbf47cada46f88494bb32554616d6
Raphaël Bournhonesque <raphael.bournhonesque@ens-lyon.fr>
2015-01-20 19:13:23 +0100

    Correction API

--- tweets.py
+++ tweets.py
@@ -1,4 +1,4 @@
-from twitter import Twitter, OAuth, TwitterStream
+from twitter import OAuth, TwitterStream
 
 credentials = {"token": "2987172311-nww55Y0ZKPKhth05wkkX88bn5z6INqQRDBq5xSX",
                "token_secret": "digi83CDHjbD8vi8W8FnyLN7t8zd56pZ1XdqATdYJivex",
@@ -16,13 +16,22 @@ class Tweet:
         self.api = None
         self.authenticated = False
 
-    def authenticate(self, credentials):
+    def authenticate(self):
         if not self.authenticated:
-            self.api = TwitterStream(auth=OAuth(*credentials))
+            self.api = TwitterStream(auth=OAuth(**self.credentials))
             self.authenticated = True
 
-    def tweet_stream(self):
+    def sample(self):
         if not self.authenticated:
             raise AccessError("You are not authenticated, please authenticate before streaming tweets.")
         else:
             return self.api.statuses.sample()
+
+
+if __name__ == "__main__":
+    tweets_grabber = Tweet(credentials)
+    tweets_grabber.authenticate()
+
+    for tweet in tweets_grabber.sample():
+        if "text" in tweet:
+            print(tweet["text"])
