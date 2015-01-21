cd12ee9232d86fd7661055c9ff3ee5319f3b2f7a
Raphaël Bournhonesque <raphael.bournhonesque@ens-lyon.fr>
2015-01-21 15:29:33 +0100

    Ajout commentaire heatmap

--- tweet_heatmap.py
+++ tweet_heatmap.py
@@ -6,7 +6,16 @@ class TweetHeat:
     def __init__(self, dbname):
         self.dbname = dbname
 
-    def save_coord(self):
+    def save_coord(self, f_name="coords"):
+        """Save the coordinates of all tweets from the database in a 'f_name' file.
+           In the file, the lat and long are separated by a whitespace, and a linebreak
+           separates each pair of coordinates.
+
+           Parameters:
+           ----------
+           f_name: str
+                  Name of the file where the coordinates must be stored.
+        """
         conn = sqlite3.connect(self.dbname)
         c = conn.cursor()
 
@@ -14,7 +23,7 @@ class TweetHeat:
                                 "FROM TWEET, PLACE "
                                 "WHERE TWEET.PLACE_ID = PLACE.PLACE_ID")
 
-        with open("coords", 'w') as output_file:
+        with open(f_name, 'w') as output_file:
             for coord in coordinates:
                 data = json.loads(coord[0])
                 output_file.write("{} {}\n".format(data[1], data[0]))
