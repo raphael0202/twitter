0e3a14e7f0f9af3ac4c96c709755a613fe59a543
Raphaël Bournhonesque <raphael.bournhonesque@ens-lyon.fr>
2015-01-21 15:48:05 +0100

    Modularisation heatmap

--- tweet_heatmap.py
+++ tweet_heatmap.py
@@ -2,9 +2,21 @@ import sqlite3
 import json
 
 
-class TweetHeat:
+class TweetCoord:
     def __init__(self, dbname):
         self.dbname = dbname
+
+    def tweet_coord(self):
+        """"""
+        conn = sqlite3.connect(self.dbname)
+        c = conn.cursor()
+
+        coordinates = c.execute("SELECT COORDINATES "
+                                "FROM TWEET, PLACE "
+                                "WHERE TWEET.PLACE_ID = PLACE.PLACE_ID")
+
+        conn.close()
+        return coordinates
 
     def save_coord(self, f_name="coords"):
         """Save the coordinates of all tweets from the database in a 'f_name' file.
@@ -16,16 +28,9 @@ class TweetHeat:
            f_name: str
                   Name of the file where the coordinates must be stored.
         """
-        conn = sqlite3.connect(self.dbname)
-        c = conn.cursor()
-
-        coordinates = c.execute("SELECT COORDINATES "
-                                "FROM TWEET, PLACE "
-                                "WHERE TWEET.PLACE_ID = PLACE.PLACE_ID")
+        coordinates = self.tweet_coord()
 
         with open(f_name, 'w') as output_file:
             for coord in coordinates:
                 data = json.loads(coord[0])
                 output_file.write("{} {}\n".format(data[1], data[0]))
-
-
